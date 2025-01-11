7ee8f316531740a9bb0f3df08b4559ad78aeae31
Joseph Demyanovskiy <48137744+KrolikPDX@users.noreply.github.com>
2025-01-11 14:06:51 -0800

    Updated comments in index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-// EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
-const { test, expect } = require('@playwright/test');
+const { expect } = require('@playwright/test');
 const { ArticlePage } = require('./pages/articlepage');
 
-async function sortHackerNewsArticles(d) {
-  // launch browser
+async function sortHackerNewsArticles() {
+  //Launch browser
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
   const articlePage = new ArticlePage(page);
   await articlePage.navigate();
-  while (articlePage.all_entries.length <= 100) { //Keep clicking more and grabbing entries until we have at least 100
+
+  //Get all entries until we have at least 100
+  while (articlePage.all_entries.length <= 100) {
     await articlePage.getEntriesFromPage();
     await articlePage.clickMoreButton();
   }
   await articlePage.trimEntriesTo(100); //Trim entries to be only first 100 
 
+  //Validate date order
   const expectedSortOrder = "Descending";
   expect(await articlePage.allEntriesSortedByDate(expectedSortOrder), `Not all entry dates were sorted by '${expectedSortOrder}'`).toBe(true); 
+  
+  //Close browser
   await page.close();
   await context.close();
   await browser.close();
